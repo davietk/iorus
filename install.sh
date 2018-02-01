9c2b82ae780171d1abaf5b0824e3a95ebf9934fe
Kevin Daviet <kevin.daviet@live.fr>
2018-02-01 13:28:04 +0100

    ajout du script de déploiement

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd /home/pi
+
+echo "Récupération du projet hzeller"
+git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
+
+echo "Déplacement du script d'affichage du texte modifié"
+mv /home/pi/rpi-rgb-led-matrix/bindings/python/samples/runtext.py /home/pi/rpi-rgb-led-matrix/bindings/python/samples/runtext.py.old
+cp /home/pi/iorus/src/runtext.py.copy /home/pi/rpi-rgb-led-matrix/bindings/python/samples/runtext.py
+
+echo "Installation des packages python"
+pip install paho-mqtt==1.2
+pip install certifi
