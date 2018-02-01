5087cca462b5ba186d0a05f040a43f78f7d830b4
Kevin Daviet <kevin.daviet@live.fr>
2018-02-01 14:06:30 +0100

    ajout des commandes de déploiement des scripts python

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ cd /home/pi
 
 echo "Récupération du projet hzeller"
 git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
+cd /home/pi/rpi-rgb-led-matrix/bindings/python
+make build-python
+sudo make install-python
 
 echo "Déplacement du script d'affichage du texte modifié"
 mv /home/pi/rpi-rgb-led-matrix/bindings/python/samples/runtext.py /home/pi/rpi-rgb-led-matrix/bindings/python/samples/runtext.py.old
