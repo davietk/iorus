e25ad3bdc2ab876f19d13ec2bd29f81d363d495c
Kevin Daviet <kevin.daviet@live.fr>
2018-03-04 17:46:00 +0100

    test gyro

--- src/test/led.py
+++ src/test/led.py
@@ -11,12 +11,14 @@ Détail technique GPIO : https://i.stack.imgur.com/yWGmW.png
 """
 
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)   # mode de numérotation des pins
-GPIO.setup(25,GPIO.OUT)  # la pin 25 réglée en sortie (output)
+GPIO.setup(21,GPIO.OUT)  # la pin 25 réglée en sortie (output)
 
 if __name__ == '__main__':
-
-	GPIO.output(25,GPIO.HIGH)   # sortie au niveau logique haut (3.3 V)
-	sleep(500)					# attente de 5 secondes
-	GPIO.output(25,GPIO.LOW)    # sortie au niveau logique bas (0 V)
+	
+	GPIO.output(21,GPIO.HIGH)   # sortie au niveau logique haut (3.3 V)
+	time.sleep(5)					# attente de 5 secondes
+	#GPIO.output(21,GPIO.LOW)    # sortie au niveau logique bas (0 V)
+	GPIO.cleanup(21)
